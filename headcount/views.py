73aa47091f43b09e079db2d9869962d9cf518ffc
italomiguelpinheiro <italo.pinheiro@ccc.ufcg.edu.br>
2024-02-15 00:10:01 -0300

    Solving the turnover question and doing improvements in code

--- headcount/views.py
+++ headcount/views.py
@@ -6,7 +6,6 @@ from django.db.models import Count
 from django.http import JsonResponse
 from datetime import datetime
 import calendar
-import json
 
 def format_date(date):
     return datetime.strptime(date, '%Y-%m-%d')
@@ -14,8 +13,29 @@ def format_date(date):
 def get_month_name(month_number):
     return calendar.month_abbr[month_number]
 
-def get_line_chart(request):
+#Retorna os funcionarios ativos por mês, o mês e o ano do periodo inidicado
+def get_actives_employees_by_period(init_date, end_date):
+    return Headcount.objects.filter(
+            fg_status=1,
+            dt_reference_month__gte=init_date,
+            dt_reference_month__lte=end_date
+        ).values('dt_reference_month__month', 'dt_reference_month__year').annotate(
+            active_employees=Count('id_employee')
+        )   
+        
+#Retorna as empresas da categoria passada e a quantidade de funcionarios ativos no mes passado no end_date
+def get_companies_and_actives_employees_on_month_by_category(end_date, category):
+    # Obs: Eu entendi que a ds_category_5 é a "categoria da base" e o ds_category_1 é o nome da empresa
+    return Headcount.objects.filter(
+            fg_status=1,
+            ds_category_5 = category,
+            dt_reference_month__month=end_date.month
+        ).values('ds_category_1').annotate(
+            active_employees=Count('id_employee')
+        ) 
+    
 
+def get_line_chart(request):
     RESPONSE_TEMPLATE =  {
         "xAxis": {
             "type": "category",
@@ -39,16 +59,9 @@ def get_line_chart(request):
     init_date = format_date(request.GET.get('init_date'))
     end_date = format_date(request.GET.get('end_date'))
 
-    filtered_data = Headcount.objects.filter(
-        fg_status=1,
-        dt_reference_month__gte=init_date,
-        dt_reference_month__lte=end_date
-    ).values('dt_reference_month__month', 'dt_reference_month__year').annotate(
-        active_employees=Count('id_employee')
-    )   
-
+    filtered_data = get_actives_employees_by_period(init_date, end_date)
+    
     agruped_data = {}
-
     for result in filtered_data:
         month = result['dt_reference_month__month']
         year = result['dt_reference_month__year']
@@ -72,7 +85,6 @@ def get_line_chart(request):
 
 
 def get_category_charts(request):
-    
     RESPONSE_TEMPLATE = {
         "xAxis": {
             "type": "value",
@@ -102,18 +114,11 @@ def get_category_charts(request):
     }
     
     end_date = format_date(request.GET.get('end_date'))
-    print(end_date.month)
     category = request.GET.get('category')
 
     #Filtrando os dados referentes aos empresários ativos que fazem parte da categoria selecionado dentro do mês determinado no end_date
     # A questão não diz nada sobre pegar os empresários ativos até o end_Date, mas sim, pegar todos os funcionarios ativos dentro do mês do end_date.
-    filtered_data = Headcount.objects.filter(
-        fg_status=1,
-        ds_category_5 = category,
-        dt_reference_month__month=end_date.month
-    ).values('ds_category_1').annotate(
-        active_employees=Count('id_employee')
-    )   
+    filtered_data = get_companies_and_actives_employees_on_month_by_category(end_date, category)
 
     for result in filtered_data:
         RESPONSE_TEMPLATE['yAxis']['data'].append(result['ds_category_1'])
--- turnover/views.py
+++ turnover/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Turnover
-from django.db.models import Count, Sum
+from django.db.models import Count
 
 from django.http import JsonResponse
 from datetime import datetime
 import calendar
-
-def format_date(date):
-    return datetime.strptime(date, '%Y-%m-%d')
-
-def get_month_name(month_number):
-    return calendar.month_abbr[month_number]
-
-def calculate_turnover(total_dismissal, active_employees, period):
-    print(total_dismissal, active_employees, period)
-    
-    # requerido pela questão: (soma de fg_demitido_no_mes) / ((contagem de id_matricula onde fg_status = 1) / (quantidade de meses do período selecionado))    
-    turnover = (total_dismissal/ (active_employees/period))
-    
-    return turnover
     
 def get_line_chart(request):
-    
-    # Foi pedido para não alterar o title, porem como sugestão creio que ficaria melhor colocar "Taxa de Turnover por período"
     RESPONSE_TEMPLATE =   {
         "xAxis": {
             "type": "category",
@@ -51,21 +35,14 @@ def get_line_chart(request):
     months = set(range(init_date.month, end_date.month + 1))
     months_names = [get_month_name(month) for month in months]
     
+    #A questão sugere que o calculo do Turnover seja: dividindo o número de funcionários que deixaram a empresa durante um PERÍODO específico pelo número médio total de funcionários durante o MESMO PERÍODO.
+    #soma de demitidos no perído / média de ativos do período
 
     #Demetidos em um determinado periodo
-    dict_dismissal_employees = Turnover.objects.filter(
-        fg_dismissal_on_month=1,
-        dt_reference_month__gte=init_date,
-        dt_reference_month__lte=end_date
-    ).aggregate(dismissal_employees=Count('id_employee'))
+    dict_dismissal_employees = get_dismissal_employees_by_period(init_date, end_date)
     
     #Ativos no mesmo período
-    dict_actives_employees = Turnover.objects.filter(
-        fg_status=1,
-        dt_reference_month__gte=init_date,
-        dt_reference_month__lte=end_date
-    ).aggregate(actives_employees=Count('id_employee'))
-    
+    dict_actives_employees = get_actives_employees_by_period(init_date, end_date)
     
     turnover = calculate_turnover(dict_dismissal_employees["dismissal_employees"],dict_actives_employees["actives_employees"], period)
 
@@ -76,15 +53,120 @@ def get_line_chart(request):
                 "data": [turnover]*len(months),
             }]
     
-    
-    # Tenho duas sugestões sobre essa questão:
-    # 1. Creio que o calculo de turnover seja diferente do descrito no enunciado. A não ser que estamos falando de taxas diferentes, o calculo do turnover é feito do seguinte modo: 
-    #    Turnover =  ((número de admissões + número de desligamentos / 2) / número total de funcionários dentro da empresa) x 100
-    # Nesse caso, a logica da questão mudaria e a tabela iria precisar ser um pouco diferente do que esta. O que nos leva a segunda sugestão:
-    # 2. Vi em populate.py que o preenchimento da tabela foi realizado de forma aleatória. Isso acabou ocasionando uma inconsistência nos dados, pois fez com que o numero de demissões fosse extremamente grande, de modo que ao multiplicarmos por 100 para obter a porcentagem o valor ficasse bem alto. 
-
+    # OBS: Vi em populate.py que o preenchimento da tabela foi realizado de forma aleatória. Isso acabou ocasionando uma inconsistência nos dados, pois fez com que o numero de demissões fosse extremamente grande, de modo que ao multiplicarmos por 100 para obter a porcentagem o valor ficasse bem alto. 
     return JsonResponse(RESPONSE_TEMPLATE)
 
  
 def get_category_charts(request):
-    return JsonResponse({'response': 'GET CATEGORY CHARTS - TURNOVER'})
+    RESPONSE_TEMPLATE = {
+        "xAxis": {
+            "type": "value",
+            "show": True,
+            "max": {}
+        },
+        "yAxis": {
+            "type": "category",
+            "data": []
+        },
+        "series": {
+            "type": "horizontal_stacked",
+            "series": []
+        },
+        "title": "Empresa",
+        "grid": 6,
+        "color": [
+            "#2896DC"
+        ],
+        "is%": False
+    }
+    
+    init_date = format_date(request.GET.get('init_date'))
+    end_date = format_date(request.GET.get('end_date'))
+    category = request.GET.get('category')
+    period = end_date.month - init_date.month + 1
+    
+    #Demitidos em um determinado periodo por empresa da categoria
+    dict_dismissal_employees = get_dismissal_employees_by_category_and_period(init_date, end_date, category)
+
+    #Ativos no mesmo período por empresa da categoria
+    dict_actives_employees = get_actives_employees_by_category_and_period(init_date, end_date, category)
+    
+    agruped_data= agrupe_queries(dict_dismissal_employees, dict_actives_employees)
+    
+    turnover_by_company = []
+    for company in agruped_data:
+        result = {}
+        result['company'] = company['company']
+        result['turnover'] = calculate_turnover(company['dismissals'], company['actives'], period)
+        turnover_by_company.append(result)
+               
+    companies, turnovers = get_companies_and_turnovers(turnover_by_company)
+    
+    RESPONSE_TEMPLATE['yAxis']['data'] = companies
+    RESPONSE_TEMPLATE['series']['series'] = [
+            {
+                "name": "Turnoveres",
+                "data": turnovers,
+                "type": "bar"
+            }
+        ]
+        
+    return JsonResponse(RESPONSE_TEMPLATE)
+
+
+def format_date(date):
+    return datetime.strptime(date, '%Y-%m-%d')
+
+def get_month_name(month_number):
+    return calendar.month_abbr[month_number]
+
+def calculate_turnover(total_dismissal, active_employees, period):
+    # requerido pela questão: (soma de fg_dismissal_on_month) / ((contagem de id_employee onde fg_status = 1) / (quantidade de meses do período selecionado))    
+    turnover = (total_dismissal/ (active_employees/period))
+    return turnover
+
+def get_dismissal_employees_by_period(init_date, end_date):
+    return Turnover.objects.filter(
+           fg_dismissal_on_month=1,
+           dt_reference_month__gte=init_date,
+           dt_reference_month__lte=end_date
+           ).aggregate(dismissal_employees=Count('id_employee'))
+
+def get_actives_employees_by_period(init_date, end_date):
+    return Turnover.objects.filter(
+           fg_status=1,
+           dt_reference_month__gte=init_date,
+           dt_reference_month__lte=end_date
+           ).aggregate(actives_employees=Count('id_employee'))
+
+#Obs: Estou considerando que o nome da empresa é representado pela coluna ds_category_1, e o nome da "categoria da base" é representado pela coluna ds_category_5
+def get_dismissal_employees_by_category_and_period(init_date, end_date, category):
+    return  Turnover.objects.filter(
+            fg_dismissal_on_month=1,
+            ds_category_5 = category,
+            dt_reference_month__gte=init_date,
+            dt_reference_month__lte=end_date
+            ).values('ds_category_1').annotate(dismissal_employees=Count('id_employee'))
+
+#Obs: Estou considerando que o nome da empresa é representado pela coluna ds_category_1, e o nome da "categoria da base" é representado pela coluna ds_category_5
+def get_actives_employees_by_category_and_period(init_date, end_date, category):
+    return Turnover.objects.filter(
+        fg_status=1,
+        ds_category_5=category,
+        dt_reference_month__gte=init_date,
+        dt_reference_month__lte=end_date
+    ).values('ds_category_1').annotate(actives_employees=Count('id_employee'))
+    
+def agrupe_queries(dismissals, actives):
+    agruped_data=[]
+    for dismissal, active in zip(dismissals, actives):
+        agruped = {}
+        agruped['company'] = dismissal['ds_category_1']
+        agruped['dismissals'] = dismissal['dismissal_employees']
+        agruped['actives'] = active['actives_employees']
+        agruped_data.append(agruped)
+    return agruped_data
+
+def get_companies_and_turnovers(turnover_by_company):
+    return zip(*[(i['company'], i['turnover']) for i in turnover_by_company])
+    
