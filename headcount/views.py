0462675daa36b667826d35fe84805399fd440d3e
italomiguelpinheiro <italo.pinheiro@ccc.ufcg.edu.br>
2024-02-15 00:41:24 -0300

    Implementing adjustments

--- headcount/views.py
+++ headcount/views.py
@@ -7,34 +7,6 @@ from django.http import JsonResponse
 from datetime import datetime
 import calendar
 
-def format_date(date):
-    return datetime.strptime(date, '%Y-%m-%d')
-
-def get_month_name(month_number):
-    return calendar.month_abbr[month_number]
-
-#Retorna os funcionarios ativos por mês, o mês e o ano do periodo inidicado
-def get_actives_employees_by_period(init_date, end_date):
-    return Headcount.objects.filter(
-            fg_status=1,
-            dt_reference_month__gte=init_date,
-            dt_reference_month__lte=end_date
-        ).values('dt_reference_month__month', 'dt_reference_month__year').annotate(
-            active_employees=Count('id_employee')
-        )   
-        
-#Retorna as empresas da categoria passada e a quantidade de funcionarios ativos no mes passado no end_date
-def get_companies_and_actives_employees_on_month_by_category(end_date, category):
-    # Obs: Eu entendi que a ds_category_5 é a "categoria da base" e o ds_category_1 é o nome da empresa
-    return Headcount.objects.filter(
-            fg_status=1,
-            ds_category_5 = category,
-            dt_reference_month__month=end_date.month
-        ).values('ds_category_1').annotate(
-            active_employees=Count('id_employee')
-        ) 
-    
-
 def get_line_chart(request):
     RESPONSE_TEMPLATE =  {
         "xAxis": {
@@ -125,3 +97,31 @@ def get_category_charts(request):
         RESPONSE_TEMPLATE['series']['series'][0]['data'].append(result['active_employees'])
         
     return JsonResponse(RESPONSE_TEMPLATE)
+
+def format_date(date):
+    return datetime.strptime(date, '%Y-%m-%d')
+
+def get_month_name(month_number):
+    return calendar.month_abbr[month_number]
+
+#Retorna os funcionarios ativos por mês, o mês e o ano do periodo inidicado
+def get_actives_employees_by_period(init_date, end_date):
+    return Headcount.objects.filter(
+            fg_status=1,
+            dt_reference_month__gte=init_date,
+            dt_reference_month__lte=end_date
+        ).values('dt_reference_month__month', 'dt_reference_month__year').annotate(
+            active_employees=Count('id_employee')
+        )   
+        
+#Retorna as empresas da categoria passada e a quantidade de funcionarios ativos no mes passado no end_date
+def get_companies_and_actives_employees_on_month_by_category(end_date, category):
+    # Obs: Eu entendi que a ds_category_5 é a "categoria da base" e o ds_category_1 é o nome da empresa
+    return Headcount.objects.filter(
+            fg_status=1,
+            ds_category_5 = category,
+            dt_reference_month__month=end_date.month
+        ).values('ds_category_1').annotate(
+            active_employees=Count('id_employee')
+        ) 
+    
